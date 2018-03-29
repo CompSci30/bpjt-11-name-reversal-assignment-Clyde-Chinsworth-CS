c651fec9068ee0462c5973248541500280ba043e
Clyde-Chinsworth-CS <a.yang2@share.epsb.ca>
2018-03-29 12:37:45 -0600

    Print name backwards

--- NameReversal.java
+++ NameReversal.java
@@ -7,12 +7,12 @@ public class NameReversal
     public static void main(String args[])
     {
         Scanner kbReader = new Scanner(System.in);
-        System.out.print("Hello, world");
+        System.out.print("Please enter your name. ");
         String name = kb.Reader.next(); //user inputs name
         name = name.toLowerCase();
         for (int j = name.length(); j >= 0; j--)
         {
-            
+            System.out.print(name.substring(j, j+1));
         }
     }
 }
